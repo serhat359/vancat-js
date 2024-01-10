e1340fefd7764306feef7d8efb0bb7f04da8daec
Serhat Abdulbakioglu <serhat359@gmail.com>
2024-01-10 22:07:42 +0900

    added some fail tests, and some success tests

--- test/test-vancat.js
+++ test/test-vancat.js
@@ -15,11 +15,18 @@ var tests = [
     ['{{a.b.c.d.e}}', { a: { b: { c: { d: { e: 'text' } } } } }, 'text'],
     ['{{a.b.c.d.e.f}}', { a: { b: { c: { d: { e: { f: 'text' } } } } } }, 'text'],
     ['hello {{upper w}}', { w: 'world' }, 'hello WORLD'],
+    ['numbers count: {{num.length}}', { num: [1, 2, 3] }, 'numbers count: 3'],
+    ['numbers count: {{text.length}}', { text: 'hello world' }, 'numbers count: 11'],
+    ['{{fixed number}}', { number: 2.762736723 }, '2.76'],
+    ['{{fixed a.b}}', { a: { b: 2.762736723 } }, '2.76'],
 ];
 
 var helpers = {
     upper: function (s) {
         return s.toUpperCase();
+    },
+    fixed: function (x) {
+        return x.toFixed(2);
     },
 };
 
@@ -27,6 +34,17 @@ for (const [template, data, expected] of tests) {
     const renderer = compile(template);
     const result = renderer(data, helpers);
     if (result !== expected) throw new Error();
+}
+
+var badCompileTests = ['{{', '{{}}', '{{x.}}', '{{x.x.}}', '{{.x}}', '{{.x.x}}', '{{x.x x.x}}'];
+
+for (let k of badCompileTests) {
+    try {
+        Templater.compile(k);
+    } catch (e) {
+        continue;
+    }
+    throw new Error(`${k} should have thrown error`);
 }
 
 console.log('SUCCESS!!!');
